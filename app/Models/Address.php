2786cc4ae6a9689d82adc6b10b35386a8e9e0ea2
keziaeci <keziaeci2806@gmail.com>
2024-05-26 13:58:55 +0700

    Address model

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -5,23 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Contact extends Model
+class Address extends Model
 {
     use HasFactory;
 
-    protected $table = 'contacts';
+    protected $table = 'addresses';
     protected $primaryKey = 'id';
     protected $keyType = 'int';
     protected $timestamps = true;
     protected $incrementing = true;
 
-    function user() : BelongsTo {
-        return $this->belongsTo(User::class , 'user_id', 'id');
-    }
-
-    function addresses() : HasMany {
-        return $this->hasMany(Address::class, 'contanct_id', 'id');
+    function contact() : BelongsTo {
+        return $this->belongsTo(Contact::class , 'contact_id', 'id');
     }
 }
